ad3491bc7f0383e6393a002712744d6890c69cdd
Chefkwami <sitsofefeyi28@gmail.com>
2025-10-17 11:53:10 +0100

    Bug fix

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get("/", (req, res) => {
+    res.send("API is live on Railway")
+})
+
 app.use("/", router);
 
 app.use(errorHandler);
